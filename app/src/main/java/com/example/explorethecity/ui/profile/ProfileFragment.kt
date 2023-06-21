d0319385a0d6096b6de8ab8fa2fba2cda65f1a02
Neslişah Çelek <neslisah.celek@outlook.com>
2023-06-21 15:13:15 +0300

    Add log out option

--- app/src/main/java/com/example/explorethecity/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/explorethecity/ui/profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.explorethecity.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,15 +8,17 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.explorethecity.MainActivity
 import com.example.explorethecity.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,6 +34,13 @@ class ProfileFragment : Fragment() {
         val textView: TextView = binding.mailTextView
         profileViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+        }
+
+        auth = Firebase.auth
+        binding.logoutButton.setOnClickListener(){
+            auth.signOut()
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
         }
         return root
     }
